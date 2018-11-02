fd9cc4541b7b6d83a6e83be5d60848aaf6e2bd97
yuhaidong <ryang2007@163.com>
2018-11-02 14:23:25 +0800

    修改测试数据代码的bug5

--- test_numpy_5_5_mnist_eval.py
+++ test_numpy_5_5_mnist_eval.py
@@ -32,7 +32,7 @@ def evaluate(mnist) :
 		# 通过变量重命名的方式来加载模型，这样在前向传播的过程中就不需要调用求滑动平均
 		# 的函数来获取平均值了。这样就可以完全共用mnist_inference.py中定义的前向传播过程
 		variable_averages = tf.train.ExponentialMovingAverage(
-							test_numpy_5_5_mnist_inference.Moving_AVERAGE_DECAY)
+							test_numpy_5_5_mnist_train.MOVING_AVERAGE_DECAY)
 		# 后注：通过variables_to_restore函数生成变量重命名字典，然后被tf.train.Saver调用，就可以直接通过变量名获得滑动平均值
 		variables_to_restore = variable_averages.variables_to_restore()
 		# 后注：重命名字典被tf.train.Saver调用
@@ -44,7 +44,7 @@ def evaluate(mnist) :
 				# tf.train.get_checkpoint_state函数会通过checkpoint文件自动找到目录中
 				# 最新模型的文件名
 				ckpt = tf.train.get_checkpoint_state(
-					mnist_train.MODEL_SAVE_PATH)
+					test_numpy_5_5_mnist_train.MODEL_SAVE_PATH)
 
 				if ckpt and ckpt.model_checkpoint_path :
 					# 加载模型
