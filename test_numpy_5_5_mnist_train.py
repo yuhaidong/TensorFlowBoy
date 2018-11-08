91fe9e24d6b6479661aa82087e98261f224c7f7e
yuhaidong <ryang2007@163.com>
2018-11-08 17:29:44 +0800

    测试输出样本值

--- test_numpy_5_5_mnist_train.py
+++ test_numpy_5_5_mnist_train.py
@@ -72,15 +72,17 @@ def train(mnist):
 		# 在训练过程中不再测试模型在验证数据上的表现，验证和测试的过程将会有一个独立的程序来完成。
 		for i in range(TRAINING_STEPS):
 			xs, ys = mnist.train.next_batch(BATCH_SIZE)
-			_, loss_value, step = sess.run([train_op, loss, global_step], 
-											feed_dict = {x: xs, y_ : ys})
+			#_, loss_value, step = sess.run([train_op, loss, global_step], 
+			#
+			aa, loss_value, step = sess.run([train_op, loss, global_step], 
+											feed_dict = {x: xs, y_ : ys})								feed_dict = {x: xs, y_ : ys})
 			# 每1000轮保存一次模型
 			if i % 10 == 0:
 				# 输出当前模型的情况。这里只输出了模型在当前训练batch上的损失函数大小。通过损失
 				# 函数的大小可以大概了解训练的情况。在验证数据集上的正确率信息会有一个单独的程序
 				# 来生成。
 				print("After %d training step(s), loss on training "
-						"batch is %g." % (step, loss_value))
+						"batch is %g, aa=%g" % (step, loss_value, aa.eval()))
 
 				# 保存当前的模型。注意这里给出了global_step参数，这样可以让每个被保存模型的文件名
 				# 末尾加上训练的轮数，比如“model.ckpt-1000”表示训练1000轮之后得到的模型
