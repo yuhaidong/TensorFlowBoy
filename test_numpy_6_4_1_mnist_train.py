1ef43bd2dbd0b02d62e9392d75b29e103f82cb1f
yuhaidong <ryang2007@163.com>
2018-11-24 23:01:43 +0800

    测试

--- test_numpy_6_4_1_mnist_train.py
+++ test_numpy_6_4_1_mnist_train.py
@@ -92,7 +92,7 @@ def train(mnist):
 
 			# _, loss_value, step = sess.run([train_op, loss, global_step], 
 			#								feed_dict = {x: xs, y_ : ys})
-			#print(reshaped_xs)
+			print(reshaped_xs[0])
 			aa, loss_value, step = sess.run([train_step, loss, global_step], 
 											feed_dict = {x: reshaped_xs, y_ : ys})
 			# 每1000轮保存一次模型
